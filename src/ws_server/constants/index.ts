1c6abb432b8fb8684ebfaa994a2e5f38f2174479
Yury Kurouski <yury_kurouski@epam.com>
2024-02-15 23:14:53 +0100

    feat: add message types

---
+++ src/ws_server/constants/index.ts
@@ -0,0 +1,8 @@
+export enum MSG_TYPE {
+  REG = 'reg',
+  UPDATE_WINNERS = 'update_winners',
+  CREATE_ROOM = 'create_room',
+  ADD_USER_TO_ROOM = 'add_user_to_room',
+  CREATE_GAME = 'create_game',
+  UPDATE_ROOM = 'update_room'
+}
