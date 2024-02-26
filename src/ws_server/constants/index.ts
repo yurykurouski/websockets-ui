7071cb209c97fe791e993a9e7681df49f76950ad
Yury Kurouski <yury_kurouski@epam.com>
2024-02-26 21:56:24 +0100

    feat: add constants

--- src/ws_server/constants/index.ts
+++ src/ws_server/constants/index.ts
@@ -4,5 +4,22 @@ export enum MSG_TYPE {
   CREATE_ROOM = 'create_room',
   ADD_USER_TO_ROOM = 'add_user_to_room',
   CREATE_GAME = 'create_game',
-  UPDATE_ROOM = 'update_room'
+  UPDATE_ROOM = 'update_room',
+  ADD_SHIPS = 'add_ships',
+  START_GAME = 'start_game',
+  TURN = 'turn',
+  ATTACK = 'attack'
+}
+
+export enum ESHIP_TYPE {
+  HUGE = 'huge',
+  MEDIUM = 'medium',
+  SMALL = 'small',
+  LARGE = 'large'
+}
+
+export enum EATTACK_STATUS {
+  MISS = "miss",
+  KILLED = "killed",
+  SHOT = "shot"
 }
